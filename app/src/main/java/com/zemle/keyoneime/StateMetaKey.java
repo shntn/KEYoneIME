56a0769ee4d0960d15add00261d2c472a3b0f6b8
shntn <qzt11222@nifty.com>
2018-01-10 23:00:56 +0900

    [clean]メタキーの状態を管理するStateMetaKeyをリファクタリング

--- app/src/main/java/com/zemle/keyoneime/StateMetaKey.java
+++ app/src/main/java/com/zemle/keyoneime/StateMetaKey.java
@@ -1,180 +1,95 @@
 package com.zemle.keyoneime;
 
-import android.view.KeyEvent;
-
-import java.util.EnumMap;
+/**
+ * メタキーの状態遷移
+ *
+ * Created by shntn on 2018/01/10.
+ */
 
 class StateMetaKey {
-    enum MetaKey {SHIFT_KEY, ALT_KEY, CTRL_KEY}
-    private enum MetaKeyState {STATE_NONE, STATE_ON, STATE_HALF_LOCKED, STATE_LOCKED}
+    private boolean CONFIG_SIMULTANEOUS = true; // 同時押しではなくても有効にする
+    private boolean CONFIG_LOCKED = true;  // 2回押すとON状態でロック
 
-    private static final EnumMap<MetaKey, Integer> KEYCODES = new EnumMap<>(MetaKey.class);
-    private static final EnumMap<MetaKey, Integer> MASK_STATES = new EnumMap<>(MetaKey.class);
+    private State state = State.OFF;
 
-    private static final int MASK_SHIFT_STATES =
-            KeyEvent.META_SHIFT_ON | KeyEvent.META_SHIFT_LEFT_ON | KeyEvent.META_SHIFT_RIGHT_ON;
-    private static final int MASK_ALT_STATES =
-            KeyEvent.META_ALT_ON | KeyEvent.META_ALT_LEFT_ON | KeyEvent.META_ALT_RIGHT_ON;
-    private static final int MASK_CTRL_STATES =
-            KeyEvent.META_CTRL_ON | KeyEvent.META_CTRL_LEFT_ON | KeyEvent.META_CTRL_RIGHT_ON;
-    private static final int MASK_ALL_STATES =
-            MASK_SHIFT_STATES | MASK_ALT_STATES | MASK_CTRL_STATES;
-
-    static {
-        KEYCODES.put(MetaKey.SHIFT_KEY, KeyEvent.KEYCODE_SHIFT_LEFT);
-        KEYCODES.put(MetaKey.ALT_KEY, KeyEvent.KEYCODE_ALT_LEFT);
-        KEYCODES.put(MetaKey.CTRL_KEY, KeyEvent.KEYCODE_SHIFT_LEFT);
-        MASK_STATES.put(MetaKey.SHIFT_KEY, MASK_SHIFT_STATES);
-        MASK_STATES.put(MetaKey.ALT_KEY, MASK_ALT_STATES);
-        MASK_STATES.put(MetaKey.CTRL_KEY, MASK_CTRL_STATES);
+    private void changeState(State next) {
+        state.exit(this);
+        state = next;
+        state.enter(this);
     }
 
-
-    private EnumMap<MetaKey, MetaKeyState> mState = new EnumMap<>(MetaKey.class);
-    private EnumMap<MetaKey, Boolean> isPressed = new EnumMap<>(MetaKey.class);
-    private EnumMap<MetaKey, Boolean> isUsed = new EnumMap<>(MetaKey.class);
-
-    private KeyController mKeyCtrl;
-
-    StateMetaKey(KeyController kc) {
-        mKeyCtrl = kc;
-        clearMetaKeyState();
+    State getCurrent() {
+        return state;
     }
 
-    void clearMetaKeyState() {
-        mKeyCtrl.clearMetaKey(MASK_ALL_STATES);
-
-        for (MetaKey m : MetaKey.values()) {
-            mState.put(m, MetaKeyState.STATE_NONE);
-            isPressed.put(m, false);
-            isUsed.put(m, false);
-        }
+    void press() {
+        state.press(this);
     }
 
-    void pressMetaKey(MetaKey key) {
-        mKeyCtrl.sendKey(KeyEvent.ACTION_DOWN, KEYCODES.get(key));
-
-        isPressed.put(key, true);
-        isUsed.put(key, false);
-        switch (mState.get(key)) {
-            case STATE_NONE:
-                mState.put(key, MetaKeyState.STATE_ON);
-                // off -> on
-                break;
-            case STATE_ON:
-                mState.put(key, MetaKeyState.STATE_LOCKED);
-                // on -> locked
-                break;
-            case STATE_HALF_LOCKED:
-                mState.put(key, MetaKeyState.STATE_LOCKED);
-                // half_locked -> locked
-                break;
-            case STATE_LOCKED:
-                mState.put(key, MetaKeyState.STATE_NONE);
-                // locked -> off
-                break;
-        }
+    void release() {
+        state.release(this);
     }
 
-    void pressSoftMetaKey(MetaKey key) {
-        mKeyCtrl.sendKey(KeyEvent.ACTION_DOWN, KEYCODES.get(key));
-        mKeyCtrl.sendKey(KeyEvent.ACTION_UP, KEYCODES.get(key));
-
-        isPressed.put(key, false);
-        isUsed.put(key, false);
-        switch (mState.get(key)) {
-            case STATE_NONE:
-                mState.put(key, MetaKeyState.STATE_HALF_LOCKED);
-                // off -> half_locked
-                break;
-            case STATE_ON:
-                mState.put(key, MetaKeyState.STATE_NONE);
-                // on -> off
-                break;
-            case STATE_HALF_LOCKED:
-                mState.put(key, MetaKeyState.STATE_NONE);
-                // half_locked -> off
-                break;
-            case STATE_LOCKED:
-                mState.put(key, MetaKeyState.STATE_NONE);
-                // locked -> off
-                break;
-        }
+    void use() {
+        state.use(this);
     }
 
-    void releaseMetaKey(MetaKey key) {
-        mKeyCtrl.sendKey(KeyEvent.ACTION_UP, KEYCODES.get(key));
-
-        isPressed.put(key, false);
-        if (mState.get(key) == MetaKeyState.STATE_ON && isUsed.get(key)) {
-            mState.put(key, MetaKeyState.STATE_NONE);
-            mKeyCtrl.clearMetaKey(MASK_STATES.get(key));
-            // on -> off
-        }
+    boolean isPress() {
+        return (state != State.OFF);
     }
 
-    int useMetaState() {
-        int meta = 0;
+    private enum State {
+        OFF {
+            void press(StateMetaKey outer) {
+                outer.changeState(ON);
+            }
+        },
+        ON {
+            void enter(StateMetaKey outer) {
+                this.used = false;
+                this.pressed = true;
+            }
 
-        if (useMetaKey(MetaKey.SHIFT_KEY)) {
-            meta |= KeyEvent.META_SHIFT_ON;
-        }
-        if (useMetaKey(MetaKey.ALT_KEY)) {
-            meta |= KeyEvent.META_ALT_ON;
-        }
-        if (useMetaKey(MetaKey.CTRL_KEY)) {
-            meta |= KeyEvent.META_CTRL_ON;
-        }
+            void exit(StateMetaKey outer) {
+                this.used = false;
+                this.pressed = false;
+            }
 
-        return meta;
-    }
-
-    boolean isPress(MetaKey key) {
-        boolean status = false;
-
-        switch (mState.get(key)) {
-            case STATE_NONE:
-                status = false;
-                break;
-            case STATE_ON:
-                status = true;
-                break;
-            case STATE_HALF_LOCKED:
-                status = true;
-                break;
-            case STATE_LOCKED:
-                status = true;
-                break;
-        }
-        return status;
-    }
-
-    private boolean useMetaKey(MetaKey key) {
-        boolean status = false;
-
-        switch (mState.get(key)) {
-            case STATE_NONE:
-                status = false;
-                break;
-            case STATE_ON:
-                if (isPressed.get(key)) {
-                    // stay on
-                    isUsed.put(key, true);
+            void press(StateMetaKey outer) {
+                if (outer.CONFIG_LOCKED) {
+                    outer.changeState(LOCKED);
                 } else {
-                    mState.put(key, MetaKeyState.STATE_NONE);
-                    mKeyCtrl.clearMetaKey(MASK_STATES.get(key));
-                    // on -> off
+                    outer.changeState(OFF);
                 }
-                status = true;
-                break;
-            case STATE_HALF_LOCKED:
-                status = true;
-                break;
-            case STATE_LOCKED:
-                status = true;
-                break;
-        }
+            }
 
-        return status;
+            void use(StateMetaKey outer) {
+                this.used = true;
+                if (!this.pressed) {
+                    outer.changeState(OFF);
+                }
+            }
+
+            void release(StateMetaKey outer) {
+                this.pressed = false;
+                if ((this.used) || (!outer.CONFIG_SIMULTANEOUS)) {
+                    outer.changeState(OFF);
+                }
+            }
+        },
+        LOCKED {
+            void press(StateMetaKey outer) {
+                outer.changeState(OFF);
+            }
+        };
+
+        boolean pressed;
+        boolean used;
+
+        void enter(StateMetaKey outer) {}
+        void exit(StateMetaKey outer) {}
+        void press(StateMetaKey outer) {}
+        void release(StateMetaKey outer) {}
+        void use(StateMetaKey outer) {}
     }
 }
